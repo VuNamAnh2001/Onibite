ddd7efcb3b31bec9afdc1a8fe5c6b901549312fd
vunamanh <namanhvu2001@gmail.com>
2025-03-13 00:38:18 +0700

    change promotion set time

--- js/main.js
+++ js/main.js
@@ -433,32 +433,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
    function makeTimer() {
-
-	   var endTime = new Date("7 March 2025 23:59:00 GMT+01:00");			
-	   endTime = (Date.parse(endTime) / 1000);
-
-	   var now = new Date();
-	   now = (Date.parse(now) / 1000);
-
-	   var timeLeft = endTime - now;
-
-	   var days = Math.floor(timeLeft / 86400); 
-	   var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-	   var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-	   var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-	   if (hours < "10") { hours = "0" + hours; }
-	   if (minutes < "10") { minutes = "0" + minutes; }
-	   if (seconds < "10") { seconds = "0" + seconds; }
-
-	   $("#days").html(days + "<span style='color: #CF0106;'>Ngày</span>");
-	   $("#hours").html(hours + "<span style='color: #CF0106;'>Giờ</span>");
-	   $("#minutes").html(minutes + "<span style='color: #CF0106;'>Phút</span>");
-	   $("#seconds").html(seconds + "<span style='color: #CF0106;'>Giây</span>");	
-
-}
-
-setInterval(function() { makeTimer(); }, 1000);
+	var endTime = new Date("7 March 2025 23:59:00 GMT+01:00");			
+	endTime = (Date.parse(endTime) / 1000);
+  
+	var now = new Date();
+	now = (Date.parse(now) / 1000);
+  
+	var timeLeft = endTime - now;
+  
+	var timerElement = $("#timer");
+	var countdownElement = $("#countdown");
+  
+	if (timeLeft <= 0) {
+	  timerElement.addClass("expired");
+	  countdownElement.html(
+		' Ưu đãi này đã kết thúc, hãy đợi ưu đãi mới trong thời gian tới bạn nhé!'
+	  );
+	} else {
+	  timerElement.removeClass("expired");
+	  var days = Math.floor(timeLeft / 86400); 
+	  var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+	  var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+	  var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+  
+	  if (hours < "10") { hours = "0" + hours; }
+	  if (minutes < "10") { minutes = "0" + minutes; }
+	  if (seconds < "10") { seconds = "0" + seconds; }
+  
+	  countdownElement.html(
+		`${days} Ngày ${hours} Giờ ${minutes} Phút ${seconds} Giây`
+	  );
+	}
+  }
+  
+  setInterval(function() { makeTimer(); }, 1000);
 
 
 
